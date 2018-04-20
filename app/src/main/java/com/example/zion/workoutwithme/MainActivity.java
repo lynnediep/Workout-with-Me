00e1b68f8673a4d7675efa8c26058474b55d829a
Zion <zion.calvo@gmail.com>
2018-04-19 18:39:59 -0700

    Testing commit

--- app/src/main/java/com/example/zion/workoutwithme/MainActivity.java
+++ app/src/main/java/com/example/zion/workoutwithme/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.zion.workoutwithme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,4 +17,8 @@ public class MainActivity extends AppCompatActivity {
     // LYNNE DIEP
     // DANIEL GUTIERREZ
     // CHUONG V TRUONG
+
+    public void onClick(View v) {
+        
+    }
 }
