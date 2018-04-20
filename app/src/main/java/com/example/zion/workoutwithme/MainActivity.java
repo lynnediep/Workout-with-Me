34291a0db739b23c63a01b15244dbeecd5c77a7f
Zion <zion.calvo@gmail.com>
2018-04-19 18:33:12 -0700

    Commited Base Project

---
+++ app/src/main/java/com/example/zion/workoutwithme/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.zion.workoutwithme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // CMPS 121 PROJECT
+    // ZION CALVO
+    // LYNNE DIEP
+    // DANIEL GUTIERREZ
+    // CHUONG V TRUONG
+}
