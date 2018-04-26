5e6743d184b62828821da5f422f84c32cacc3da1
lynnediep <lytdiep@ucsc.edu>
2018-04-25 17:09:09 -0700

    Profile Edit java

---
+++ app/src/main/java/com/example/zion/workoutwithme/Profile_Edit.java
@@ -0,0 +1,17 @@
+package com.example.zion.workoutwithme;
+
+// Profile Edit Java
+// Lynne Diep
+// Chuong Truong
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profile_Edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile__edit);
+    }
+}
