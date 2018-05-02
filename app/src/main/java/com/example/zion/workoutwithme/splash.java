0dcb0182b52728780aa5bea1212d66221ad0204e
daadguti <daadguti@ucsc.edu>
2018-05-01 22:52:35 -0700

    this is a quck splash page

---
+++ app/src/main/java/com/example/zion/workoutwithme/splash.java
@@ -0,0 +1,25 @@
+package com.example.zion.workoutwithme;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splash.this, Log_In.class));
+                finish();
+
+            }
+        }, 3000);
+    }
+}
